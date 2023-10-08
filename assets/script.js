28e85e29742d17482878454c958cbd5129db6f3e
Devin Shade <devinshade@Devins-MacBook-Pro.local>
2023-10-08 13:12:36 -0600

    updated js

--- assets/script.js
+++ assets/script.js
@@ -4,30 +4,13 @@ THEN a timer starts and I am presented with a question
 */
 
 // TODO: When I click the start button then a timer starts
+const btnStart = document.getElementById("btn-start")
 
-let counter = 75;
-let timeout;
-let timer_on = 0;
-let timer = document.getElementById("timer")
 
-function timedCount() {
-    timer.textContent = counter
-    document.querySelector("btn-start").onclick = counter;
-    document.getElementById("timer").value = counter;
-    --counter;
-    // timeout = setTimeout(timedCount, 1000);
-}
+btnStart.addEventListener("click", startQuiz)
+function startQuiz() {
+    // console.log("AVOCADO")
 
-function startCount() {
-  if (!timer_on) {
-    timer_on = 1;
-    timedCount();
-  }
-}
-
-function stopCount() {
-  clearTimeout(timeout);
-  timer_on = 0;
 }
 
 // TODO: When I click the start button then I'm presented with a question
