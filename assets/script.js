ba5617e77398fd678f6f89656f618344b2da2278
Devin Shade <devinshade@Devins-MacBook-Pro.local>
2023-10-07 20:23:12 -0600

    updated css

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,26 @@ THEN a timer starts and I am presented with a question
 
 // TODO: When I click the start button then a timer starts
 
+let counter = 0;
+let timeout;
+let timer_on = 0;
 
+function timedCount() {
+  document.getElementById("countdown").value = counter;
+  counter++;
+  timeout = setTimeout(timedCount, 1000);
+}
+
+function startCount() {
+  if (!timer_on) {
+    timer_on = 1;
+    timedCount();
+  }
+}
+
+function stopCount() {
+  clearTimeout(timeout);
+  timer_on = 0;
+}
 
 // TODO: When I click the start button then I'm presented with a question
