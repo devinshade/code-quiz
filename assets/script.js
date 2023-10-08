3415139c9e0f6228bccd8cdbd3b232ac924c406a
Devin Shade <devinshade@Devins-MacBook-Pro.local>
2023-10-07 18:00:13 -0600

    added css

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+/* 
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+*/
+
+// TODO: When I click the start button then a timer starts
+
+
+
+// TODO: When I click the start button then I'm presented with a question
