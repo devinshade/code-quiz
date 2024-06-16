e8fbd66a84c69799f9ab7b99fe33a9e94e3e6314
devinshade <devindanielle93@gmail.com>
2024-06-15 21:39:18 -0600

    continued final container and added TODOs for next time

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,8 @@ const startBtn = document.getElementById("start-btn");
 const nextBtn = document.getElementById("next-btn");
 const restartBtn = document.getElementById("restart-btn");
 const resultDiv = document.getElementById("result");
+const finalContainer = document.getElementById("final-container");
+// TODO: import high score and make only visible when link is clicked
 
 let shuffledQuestions, currentQuestionsIndex, score;
 
@@ -65,6 +67,7 @@ startState();
 function startState() {
     startContainer.style.display = "flex";
     questionContainer.style.display = "none";
+    finalContainer.style.display = "none";
     nextBtn.classList.add("hide");
     restartBtn.classList.add("hide");
     startDiv.classList.remove("hide");
@@ -80,11 +83,11 @@ startBtn.addEventListener("click", () => {
 });
 
 // ! TODO: When I click the start button then I'm presented with a question
-
 function startQuiz() {
     score = 0;
     questionContainer.style.display = "flex";
     startContainer.style.display = "none";
+    finalContainer.style.display = "none";
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionsIndex = 0;
     nextBtn.classList.remove("hide");
@@ -98,11 +101,12 @@ function startQuiz() {
     setNextQuestion();
     
     var timerValue = 75;
+    // TODO: not working - timer doesn't decrease by 10 with wrong answers
     function updateCountdown() {
         const countdownEl = document.getElementById("timer");
         countdownEl.textContent = timerValue;
         --timerValue;
-
+    // TODO: continues counting upon completing quiz
         if (timerValue < 0) {
             clearInterval(countdownInterval);
             countdownEl.textContent = "Failed - try again!";
@@ -110,7 +114,7 @@ function startQuiz() {
     }
     
     updateCountdown();
-
+    
     const countdownInterval = setInterval(updateCountdown, 1000);
 };
 
@@ -170,9 +174,11 @@ nextBtn.addEventListener("click", () => {
 restartBtn.addEventListener("click", startQuiz);
 
 function endQuiz() {
+    finalContainer.style.display = "flex";
     questionContainer.style.display = "none";
     nextBtn.classList.add("hide");
     restartBtn.classList.remove("hide");
     resultDiv.classList.remove("hide");
     resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`
 }
+
